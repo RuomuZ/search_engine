487f8f83116fde9680a35581e247f56595335594
RuomuZ <1206757035@qq.com>
2024-02-22 22:08:34 -0800

    porter done

--- porter.py
+++ porter.py
@@ -5,10 +5,15 @@ v_wxy = ['a','e','i','o','u','x','Y','z']
 valid_LI = ['c','d','e','g','h','k','m','n','r','t']
 db = ['b','d','f','g','m','n','p','r','t']
 p_ex = ['gener','commun','arsen']
-step2_sf = []
-
+except1 = {"skis":"ski","skies":"sky","dying":"die","lying":"lie","tying":"tie","idly":"idl","gently":"gentl","ugly":"ugli",
+        "early":"earli","only":"onli","singly":"singl", "sky":"sky","news":"news","howe":"howe","atlas":"atlas","cosmos":"cosmos",
+        "bias":"bias","andes":"andes"}
+except2 = ["inning","outing","canning","herring","earring","proceed","exceed","succeed"]
 
 def porter(t):
+    if t in except1:
+        print(f"word {t} is in except1")
+        return except1[t]
     wl = list(t)
     r1 = None
     r2 = None
@@ -34,13 +39,22 @@ def porter(t):
                     break
     #print(r1, r2)
     wl = backwardmode(wl, r1, r2)
-
+    #print(f"final: {wl}")
+    return "".join(wl)
 
 def backwardmode(wl, r1, r2):
     wl = step1a(wl)
+    if "".join(wl) in except2:
+        print(f"word {wl} is in except 2")
+        return "".join(wl)
     wl = step1b(wl,r1)
     wl = step1c(wl)
     wl = step2(wl, r1)
+    wl = step3(wl,r1,r2)
+    wl = step4(wl,r2)
+    wl = step5(wl,r1,r2)
+    return wl
+
 
 def step1a(wl):
     t = "".join(wl)
@@ -49,8 +63,13 @@ def step1a(wl):
         t = re.sub("(ied|ies)$", "ie", t)
     elif len(t) > 4:
         t = re.sub("(ied|ies)$", "i", t)
-    if t[-1] == 's' and len(t) > 1 and (t[-2] not in v) and (t[-2] != 's'):
+    count_v = 0
+    for i in t:
+        if i in v:
+            count_v += 1
+    if t[-1] == 's' and len(t) > 1 and ((t[-2] not in v) or count_v >= 2) and (t[-2] != 's'):
         t = t[0:-1]
+    #print(f"after 1a: {t}")
     return list(t)
 
 def step1b(wl,r1):
@@ -75,6 +94,7 @@ def step1b(wl,r1):
     if isShort(t,r1):
         t = t + "e"
     #print(t)
+    #print(f"after 1b: {t}")
     return list(t)
 
 
@@ -83,7 +103,7 @@ def isShort(t,r1):
         return False
     if len(t) == 2 and t[0] in v and t[1] not in v:
         return True
-    elif len(t) > 2 and t[-1] not in v_wxy and t[-2] in v and t[-3] not in v:
+    elif len(t) == 3 and t[-1] not in v_wxy and t[-2] in v and t[-3] not in v:
         return True
     else:
         return False
@@ -92,6 +112,8 @@ def step1c(wl):
     if len(wl) > 2 and (wl[-1] == 'y' or wl[-1] == "Y") and wl[-2] not in v:
         wl[-1] = "i"
     #print("".join(wl))
+    t = "".join(wl)
+    #print(f"after 1c: {t}")
     return wl
 
 
@@ -99,13 +121,13 @@ def step2(wl,r1):
     if r1 == None:
         return wl
     t = "".join(wl)
-    print(t + " and its r1 index" + str(r1))
+    #print(t + " and its r1 index" + str(r1))
     pre = t[0:r1]
-    print("pre: " + pre)
+    #print("pre: " + pre)
     region1 = t[r1:]
-    print("region1: " + region1)
+    #print("region1: " + region1)
     region1 = re.sub("tional$", "tion", region1)
-    print("region1: after " + region1)
+    #print("region1: after " + region1)
     region1 = re.sub("enci$", "ence", region1)
     region1 = re.sub("anci$", "ance", region1)
     region1 = re.sub("abli$", "able", region1)
@@ -124,11 +146,85 @@ def step2(wl,r1):
     if len(t) >= 3 and t[-3] in valid_LI:
         region1 = re.sub("li$", "", region1)
     t = pre + region1
-    print(t)
-    return t
+    #print(t)
+    #print(f"word after step2: {t}")
+    return list(t)
 
 
-porter("hopelessly")
+def step3(wl,r1, r2):
+    if r1 == None:
+        return wl
+    t = "".join(wl)
+    pre = t[0:r1]
+    #print("pre: " + pre)
+    region1 = t[r1:]
+    is_r2_modified = False
+    region2 = None
+    if r2 != None:
+        region2 = t[r2:]
+    #print("region1: " + region1)
+    region1 = re.sub("tional$", "tion", region1)
+    #print("region1: after " + region1)
+    region1 = re.sub("ational$", "ate", region1)
+    region1 = re.sub("alize$", "al", region1)
+    region1 = re.sub("(icate|iciti|ical)$", "ic", region1)
+    region1 = re.sub("(full|ness)$", "", region1)
+    if region2 != None and re.search("ative$",region2):
+        region2 = re.sub("ative$","",region2)
+        is_r2_modified = True
+    if is_r2_modified:
+        t = t[0:r2] + region2
+    else:
+        t = pre + region1
+    #print(f"word after step3: {t}")
+    return list(t)
+
+
+def step4(wl,r2):
+    if r2 == None:
+        return wl
+    t = "".join(wl)
+    pre = t[0:r2]
+    #print("pre: " + pre)
+    region2 = t[r2:]
+    #print("region1: " + region1)
+    region2 = re.sub("(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ism|ate|iti|ous|ive|ize)$", "", region2)
+    if len(t) >= 4 and (t[-4] == "s" or t[-4] == "t"):
+        region2 = re.sub("ion$", "", region2)
+    t = pre + region2
+    #print(f"word after step4: {t}")
+    return list(t)
+
+def step5(wl,r1,r2):
+    if r1 == None:
+        return wl
+    t = "".join(wl)
+    pre = t[0:r1]
+    #print("pre: " + pre)
+    region1 = t[r1:]
+    is_r2_modified = False
+    region2 = None
+    #print(f"{t} : {r2}")
+    if r2 != None:
+        region2 = t[r2:]
+    if region2 != None and (re.search("e$", region2) or re.search("ll$",region2)):
+        region2 = region2[0:-1]
+        is_r2_modified = True
+    elif len(t) >= 4 and t[-1] == 'e' and  not (t[-2] not in v_wxy and t[-3] in v and t[-4] not in v):
+        region1 = region1[0:-1] 
+    if is_r2_modified:
+        t = t[0:r2] + region2
+    else:
+        t = pre + region1
+    t = t.lower()
+    #print(f"word after step5: {t}")
+    return list(t)
+
+
+
+
+
+porter("sky")
 #porter("cries")
 #porter("ties")
 #porter("tied")
