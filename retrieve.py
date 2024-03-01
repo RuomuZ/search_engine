d2fa6831d1dbc29010515c14368db5d810fb8f4c
RuomuZ <1206757035@qq.com>
2024-03-01 15:14:35 -0800

    boolean retr done, web GUI improved, indexer bug fixed

---
+++ retrieve.py
@@ -0,0 +1,75 @@
+import os
+import re
+import porter
+
+
+def parse(post_str_list):
+    toReturn = []
+    for i in post_str_list:
+        temp = []
+        for post in i.split("|"):
+            b = post.split(".")
+            b[1] = b[1].strip('][').split(', ')
+            for l in range(len(b[1])):
+                b[1][l] = int(b[1][l])
+            b[2] = int(b[2])
+            temp.append(b)
+        toReturn.append(temp)
+    return toReturn
+
+
+
+def df_match(doc_id,list_slice):
+    if len(list_slice) == 1:
+        index = 0
+        while int(list_slice[0][index][0]) <= int(doc_id):
+            if int(list_slice[0][index][0]) == int(doc_id):
+                return True
+            index += 1
+        return False
+    else:
+        if df_match(doc_id,list_slice[0:1]) and df_match(doc_id,list_slice[1:]):
+            return True
+        else:
+            return False
+
+
+#pl -> position, il -> index list, q -> query
+
+def bool_retr(p, il, q):
+    myP = r"[a-z0-9]{2,23}"
+    target_list = re.findall(myP,q.lower())
+    post_list = []
+    for i in range(len(target_list)):
+        target_list[i] = porter.porter(target_list[i])
+    token_set = set(target_list)
+    print(token_set)
+    for token in token_set:
+        token_init = ord(token[0])
+        if token in p and token_init >= 97 and token_init <= 122:
+            il[token_init - 97].seek(p[token])
+            post_list.append(il[token_init - 97].readline().strip())
+        elif token in p:
+            il[26].seek(p[token])
+            post_list.append(il[26].readline().strip())
+    post_list = parse(post_list)
+    #print(post_list)
+    post_list.sort(key=lambda x : len(x))
+    toReturn = []
+    counter = 0
+    if len(post_list) == 1:
+        temp = post_list[0][:10]
+        print(f"temp: {temp}")
+        for i in temp:
+            toReturn.append(i[0])
+        return toReturn
+    elif len(post_list) > 1:
+        for post in post_list[0]:
+            if df_match(post[0],post_list[1:]):
+                toReturn.append(post[0])
+                counter += 1
+            if counter >= 10:
+                print(toReturn)
+                return toReturn
+    print(toReturn)
+    return toReturn
--- server.py
+++ server.py
@@ -5,15 +5,46 @@ import json
 import os
 import sys
 import io 
-import exp
+import retrieve
 
 PORT = 8000
+initChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y',
+        'z','other']
+position = {}
+fl = []
+url = {}
+
+
+def init():
+    global fl, position, url
+    fl = []
+    url = {}
+    position = {}
+    with open("finalIndex/position.json","r") as f:
+        position = json.load(f)
+    with open("finalIndex/url_id_match.json","r") as f:
+        url = json.load(f)
+    for c in initChar:
+        f = open(f"finalIndex/{c}.txt","r")
+        fl.append(f)
+
+
+def end():
+    global fl
+    for i in range(len(fl)):
+        fl[i].close()
+
 
 class myHttpHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self,request, client_address, server, directory=None):
         super().__init__(request,client_address,server,directory=None)
 
     def do_GET(self):
+        global url,position,fl
+        q = urlparse(self.path).query
+        doc_ids = []
+        if q != '':
+            doc_ids = retrieve.bool_retr(position,fl,str((parse_qs(q))["search"][0]))
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
@@ -24,15 +55,21 @@ class myHttpHandler(http.server.SimpleHTTPRequestHandler):
        "<input name='search' placeholder=''  type='text'>" +
       "</label></form>")
         output.write("<table><thead><tr><th>Title</th></tr>")
-        output.write("<tr><th>"+exp.diy("blah")+"</th></tr>")
+        for doc_id in doc_ids:
+            output.write("<tr><th>"+url[doc_id]+"</th></tr>")
         output.write("</thead></table></body></html>")        
         self.wfile.write(output.getvalue().encode())
         return
 
 
-
 Handler = myHttpHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+if __name__ == "__main__":
+    init()
+    try:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            httpd.serve_forever()
+    except:
+        end()
+        raise
+    end()
